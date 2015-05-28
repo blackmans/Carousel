becf2d3539834d015147ba32c11f7f08129dcb4a
guoqun.zhang <guoqun.zhang@126.com>
2015-05-28 11:23:23 +0800

    edit Slider

--- src/scripts/Slider.js
+++ src/scripts/Slider.js
@@ -4,13 +4,32 @@
  * @date    2015-05-26 17:20:34
  * @version 0.0.1
  */
-function Slider(opts){}
+/*
+参数：
+	元素节点：node
+方法：
+	是否自动播放：autoPlay();
+	
+*/
+function Slider(opts){
+	this.options = TOOLS.extend({
+		node: ''
+	}, opts);
+}
+
 
 Slider.prototype = {
 	constructor: Slider,
-	init: function(){}
+	init: function(){},
+	initDom: function(),
+	render: function(){}
 }
 
-var TOOlS = {
-	extend: function(){}
+var TOOLS = {
+	extend: function(obj1, obj2){
+		for(var key in obj2){
+			obj1[key] = obj2[key];
+		}
+		return obj1;
+	}
 }
