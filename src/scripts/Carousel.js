8bb189f8d20c9755f49b0aeb12c1ab39c011173d
guoqun.zhang <guoqun.zhang@126.com>
2015-05-28 19:40:18 +0800

    edit Carousel

--- src/scripts/Carousel.js
+++ src/scripts/Carousel.js
@@ -8,7 +8,7 @@
 名称：图片轮播组件
 
 参数：
-	元素节点：node
+	元素节点：node 类型：js Dom对象
 	每次左右移动的距离：width
 	每次上下移动的距离：height
 	图片轮播的时间间隔：interval
@@ -19,7 +19,12 @@
 */
 function Carousel(opts){
 	this.options = TOOLS.extend({
-		node: ''
+		node: null,
+		imgNode: null,
+		btnNode: null,
+		imgEle: null,
+		btnEle: null,
+		interval: 3000
 	}, opts || {});
 }
 
@@ -28,10 +33,9 @@ Carousel.prototype = {
 	constructor: Carousel,
 	init: function(){},
 	initDom: function(),
-	render: function(){
-		
-	},
-
+	render: function(){},
+	initUI: function(){},
+	bind: function(){}
 }
 
 var TOOLS = {
